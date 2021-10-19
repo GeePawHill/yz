124b5d58178342eebe4538e98128de9d843b6be7
GeePaw Hill <geepawhill@geepawhill.org>
2021-10-19 13:19:23 -0400

    Add Making configuration, convince gradle to find my main correctly.

--- src/main/kotlin/org/geepawhill/yz/Main.kt
+++ src/main/kotlin/org/geepawhill/yz/Main.kt
@@ -1,7 +1,12 @@
 package org.geepawhill.yz
 
-import tornadofx.*
+import tornadofx.App
+import tornadofx.launch
 
 class Main : App(YzView::class) {
 }
 
+fun main(args: Array<String>) {
+    println("Called.")
+    launch<Main>(args)
+}
