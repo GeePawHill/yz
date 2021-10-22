f2ec1e1038794500b035174cc8c009b725e2f709
GeePaw Hill <geepawhill@geepawhill.org>
2021-10-22 09:55:25 -0400

    Failing MakingModelTest

--- src/main/kotlin/org/geepawhill/yz/making/MakingModel.kt
+++ src/main/kotlin/org/geepawhill/yz/making/MakingModel.kt
@@ -1,2 +1,8 @@
-package org.geepawhill.yz.making 
+package org.geepawhill.yz.making
 
+import com.google.common.eventbus.EventBus
+import tornadofx.*
+
+class MakingModel(val bus: EventBus) {
+    val events = observableListOf<Any>()
+}
--- src/test/kotlin/org/geepawhill/yz/MakingModelTest.kt
+++ src/test/kotlin/org/geepawhill/yz/MakingModelTest.kt
@@ -1,4 +1,18 @@
 package org.geepawhill.yz
 
+import com.google.common.eventbus.EventBus
+import org.assertj.core.api.Assertions.assertThat
+import org.geepawhill.yz.making.MakingModel
+import org.junit.jupiter.api.Test
+
 class MakingModelTest {
+
+    val bus = EventBus()
+    val model = MakingModel(bus)
+
+    @Test
+    fun `catches pipchange events`() {
+        bus.post(PipChange(0, 0))
+        assertThat(model.events).contains(PipChange(0, 0))
+    }
 }
